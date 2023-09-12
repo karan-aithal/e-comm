36a12e09806d4cfcd0b1632d61e71d54da99f566
karan-aithal <karanaithal841@gmail.com>
2023-09-12 12:04:38 +0530

    Initial 2

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import BgVid from '../assets/Videobg.mp4'
+import '../components/Main.scss'
+import $ from 'jquery';
+
+
+//creates an HTML canvas which uses WebGL to
+// render spinning confetti using JavaScript.
+//const canvas =  document.getElementById("ripple") as HTMLCanvasElement;
+//const gl = canvas.getContext("webgl");// Tell the canvas element that we will use WebGL to draw
+// inside the element (and not the default raster engine):
+//const ext =  gl.getExtension("OES_texture_float") ||
 
 function Main() {
+
+  // useEffect(() => {
+  //   const script = document.createElement('script');
+  //   script.src = "node_modules/ripple-js/ripple.min.js";
+  //   script.async = true;
+  //   document.body.appendChild(script);
+    //  $("ripple").ripples({
+    //    resolution: 512,
+    //    dropRadius: 20,
+    //    perturbance: 0.01,
+    //  });
+  //   return () => {
+  //     document.body.removeChild(script);
+  //   }
+  // }, []);
+
+
   return (
-    <div>Main</div>
+    
+    <div className='BgVideo'>
+        <div id="ripple">       
+          <video src={BgVid} autoPlay loop muted></video>
+        </div>
+      </div>
   )
 }
 
 export default Main
+
+
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,3 +1,21 @@
 /// <reference types="react-scripts" />
 
+declare const canvas: HTMLCanvasElement;
+declare const existingCanvas: HTMLElement | null;
+declare const gl: WebGLRenderingContext | null;
 
+
+declare module "*.mp4" {
+    const src: string;
+    export default src;
+  }
+  
+  declare module "*.gif" {
+    const src: string;
+    export default src;
+  }
+  
+  declare module "*.jpg" {
+    const src: string;
+    export default src;
+  }
